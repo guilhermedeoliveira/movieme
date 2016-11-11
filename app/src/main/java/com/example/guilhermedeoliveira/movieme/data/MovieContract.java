15066a66df464f119ee2433e23c0b45fa60272b3
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-11-11 18:48:39 -0200

    Init Content Provider

--- app/src/main/java/com/example/guilhermedeoliveira/movieme/data/MovieContract.java
+++ app/src/main/java/com/example/guilhermedeoliveira/movieme/data/MovieContract.java
@@ -1,5 +1,8 @@
 package com.example.guilhermedeoliveira.movieme.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,7 +11,25 @@ import android.provider.BaseColumns;
 
 public class MovieContract {
 
+    // The name of unique content provider (Authority)
+    public static final String CONTENT_AUTHORITY = "com.example.guilhermedeoliveira.movieme";
+    // Create the base URI
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    // Path to access tables
+    public static final String PATH_MOVIE = "movie";
+    public static final String PATH_REVIEW = "review";
+    public static final String PATH_VIDEO = "video";
+
     public static final class MovieEntry implements BaseColumns {
+
+        // Content URI for table MovieEntry
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
 
         public static final String TABLE_NAME = "movie";
 
@@ -20,9 +41,61 @@ public class MovieContract {
         public static final String COLUMN_RELEASE_DATE = "release_date";
         public static final String COLUMN_FAVORITE = "favorite";
 
+        /**
+         * This method creates a URI for addressing a movie according to its poster URL
+         *
+         * @param posterUrl The stringly-typed URL fetched from the cloud service
+         * @return The URI with the given {@code posterUrl} appended
+         */
+        public static Uri buildMovieWithPoster(String posterUrl) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(posterUrl.substring(1)) //remove the heading slash
+                    .build();
+        }
+
+        /**
+         * Build a Uri for a record of the table, using the ID
+         *
+         * @param id The ID of the record
+         * @return A new Uri with the given ID appended to the end of the path
+         */
+        public static Uri buildMovieWithId(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        /**
+         * This method does the opposite of {@code buildMovieWithPoster}, hence returns the
+         * stringly-typed URL
+         *
+         * @param uri The URI of the movie
+         * @return The poster URL fetched from the URI
+         */
+        public static String getPosterUrlFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        /**
+         * Parse the ID of a record, or return -1 instead
+         *
+         * @param uri The Uri of the record
+         * @return The Id of the record or -1 if this doesn't apply
+         */
+        public static long getIdFromUri(Uri uri) {
+            return ContentUris.parseId(uri);
+        }
+
     }
 
     public static final class ReviewEntry implements BaseColumns {
+
+        // Content URI for table ReviewEntry
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_REVIEW).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEW;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEW;
 
         public static final String TABLE_NAME = "review";
 
@@ -31,9 +104,39 @@ public class MovieContract {
         public static final String COLUMN_AUTHOR = "author";
         public static final String COLUMN_CONTENT = "content";
         public static final String COLUMN_URL = "url";
+
+        /**
+         * Get the movie ID in the URI (the ID from the Backend)
+         *
+         * @param uri The Uri of the review with the movie id appended
+         * @return The ID of the movie, or -1 if doesn't exist
+         */
+        public static long getMovieIdFromUri(Uri uri) {
+            return ContentUris.parseId(uri);
+        }
+
+        /**
+         * Creates a trailer uri with the movie id (from the backend) appended
+         *
+         * @param insertedId The ID of the movie
+         * @return The uri of the review
+         */
+        public static Uri buildVideoWithId(long insertedId) {
+            return ContentUris.withAppendedId(CONTENT_URI, insertedId);
+        }
+
     }
 
     public static final class VideoEntry implements BaseColumns {
+
+        // Content URI for table VideoEntry
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_VIDEO).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_VIDEO;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_VIDEO;
 
         public static final String TABLE_NAME = "video";
 
@@ -41,6 +144,26 @@ public class MovieContract {
         public static final String COLUMN_MOVIE_ID = "movie_id";
         public static final String COLUMN_YOUTUBE_KEY = "youtube_key";
         public static final String COLUMN_TITLE_VIDEO = "name";
+
+        /**
+         * Get the movie ID in the URI (the ID from the Backend)
+         *
+         * @param uri The trailer's URI with the movie ID
+         * @return The movie ID or -1 if doesn't exist
+         */
+        public static long getMovieIdFromUri(Uri uri) {
+            return ContentUris.parseId(uri);
+        }
+
+        /**
+         * Creates a trailer uri with the movie id (from the backend) appended
+         *
+         * @param movieId The movie ID
+         * @return the URI of the trailer
+         */
+        public static Uri buildVideoWithId(long movieId) {
+            return ContentUris.withAppendedId(CONTENT_URI, movieId);
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/guilhermedeoliveira/movieme/data/MovieProvider.java
@@ -0,0 +1,406 @@
+package com.example.guilhermedeoliveira.movieme.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+/**
+ * Created by guilhermedeoliveira on 10/11/16.
+ */
+
+public class MovieProvider extends ContentProvider {
+
+    // The URI Matcher used by this content provider.
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mOpenHelper;
+
+    public static final int MOVIE = 100;
+    public static final int MOVIE_WITH_POSTER = 101;
+    public static final int MOVIE_WITH_ID = 102;
+
+    public static final int REVIEW = 200;
+    public static final int REVIEW_WITH_MOVIE_ID = 201;
+
+    public static final int VIDEO = 300;
+    public static final int VIDEO_WITH_MOVIE_ID = 301;
+
+    static UriMatcher buildUriMatcher() {
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, MovieContract.PATH_MOVIE, MOVIE);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE + "/#", MOVIE_WITH_ID);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE + "/*", MOVIE_WITH_POSTER);
+
+        matcher.addURI(authority, MovieContract.PATH_REVIEW, REVIEW);
+        matcher.addURI(authority, MovieContract.PATH_REVIEW + "/#", REVIEW_WITH_MOVIE_ID);
+
+        matcher.addURI(authority, MovieContract.PATH_VIDEO, VIDEO);
+        matcher.addURI(authority, MovieContract.PATH_VIDEO + "/#", VIDEO_WITH_MOVIE_ID);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // Use the Uri Matcher to determine what kind of URI this is:
+        // TYPE OR ITEM_TYPE
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MOVIE:
+                return MovieContract.MovieEntry.CONTENT_TYPE;
+
+            case MOVIE_WITH_POSTER:
+                return MovieContract.MovieEntry.CONTENT_ITEM_TYPE;
+
+            case MOVIE_WITH_ID:
+                return MovieContract.MovieEntry.CONTENT_ITEM_TYPE;
+
+            case REVIEW:
+                return MovieContract.ReviewEntry.CONTENT_TYPE;
+
+            case REVIEW_WITH_MOVIE_ID:
+                return MovieContract.ReviewEntry.CONTENT_ITEM_TYPE;
+
+            case VIDEO:
+                return MovieContract.VideoEntry.CONTENT_TYPE;
+
+            case VIDEO_WITH_MOVIE_ID:
+                return MovieContract.VideoEntry.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    /**
+     * Here's the switch statement that, given a URI, will determine what kind of request it is,
+     * and query the database accordingly.
+     *
+     * @param uri           The Uri of the data queried, containing information about where to search
+     * @param projection    An array of required columns from the table ({@code null} for all columns)
+     * @param selection     The where clause
+     * @param selectionArgs The arguments of the where clause
+     * @param sortOrder     The order of sorting the movies
+     * @return cursor
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        Cursor cursor;
+        long _id;
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE: // Movie
+                cursor = db.query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case MOVIE_WITH_POSTER: // Movie/#
+                String posterUrl = MovieContract.MovieEntry.getPosterUrlFromUri(uri);
+                cursor = db.query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        MovieContract.MovieEntry.COLUMN_POSTER + " = ?",
+                        new String[]{posterUrl},
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case MOVIE_WITH_ID: // Movie/*
+                _id = MovieContract.MovieEntry.getIdFromUri(uri);
+                cursor = db.query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        MovieContract.MovieEntry._ID + " = ?",
+                        new String[]{Long.toString(_id)},
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case REVIEW: // Review
+                cursor = db.query(
+                        MovieContract.ReviewEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case REVIEW_WITH_MOVIE_ID: // Review/#
+                _id = MovieContract.ReviewEntry.getMovieIdFromUri(uri);
+                cursor = db.query(
+                        MovieContract.ReviewEntry.TABLE_NAME,
+                        projection,
+                        MovieContract.ReviewEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{Long.toString(_id)},
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case VIDEO: // Video
+                cursor = db.query(
+                        MovieContract.VideoEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case VIDEO_WITH_MOVIE_ID: // Video/#
+                _id = MovieContract.VideoEntry.getMovieIdFromUri(uri);
+                cursor = db.query(
+                        MovieContract.VideoEntry.TABLE_NAME,
+                        projection,
+                        MovieContract.VideoEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{Long.toString(_id)},
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    /**
+     * Insert a new record into the table pointed by the Uri
+     *
+     * @param uri    The Uri that should point into a table
+     * @param values Record to add into the table
+     * @return A Content Uri with the ID of the inserted row
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        Uri returnUri;
+        long _id;
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE:
+                _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+                if (_id > 0) {
+                    returnUri = MovieContract.MovieEntry.buildMovieWithId(_id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+
+            case REVIEW:
+                _id = db.insert(MovieContract.ReviewEntry.TABLE_NAME, null, values);
+                if (_id > 0) {
+                    returnUri = MovieContract.ReviewEntry.buildVideoWithId(_id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+
+            case VIDEO:
+                _id = db.insert(MovieContract.VideoEntry.TABLE_NAME, null, values);
+                if (_id > 0) {
+                    returnUri = MovieContract.VideoEntry.buildVideoWithId(_id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    /**
+     * Deletes the records in the table pointed by the URI and match the other args
+     *
+     * @param uri           Uri of the table where to search the records to delete
+     * @param selection     A selection criteria to apply when filtering rows. If {@code null} then all
+     *                      rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by the values
+     *                      from selectionArgs, in order that they appear in the selection. The values
+     *                      will be bound as Strings.
+     * @return The number of deleted rows
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int rowsDeleted;
+
+        // Deletes all rows return the number of rows deleted
+        if (selection == null) {
+            selection = "1";
+        }
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE:
+                rowsDeleted = db.delete(MovieContract.MovieEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case REVIEW:
+                rowsDeleted = db.delete(MovieContract.ReviewEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case VIDEO:
+                rowsDeleted = db.delete(MovieContract.VideoEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // Notify only if there is rows deleted
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
+    }
+
+    /**
+     * Updates a record on the table pointed by the URI.
+     *
+     * @param uri           Uri of the table where to search the records to update
+     * @param values        The new set of {@code ContentValues} to replace the old ones
+     * @param selection     A selection criteria to apply when filtering rows. If {@code null} then all
+     *                      rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by the values
+     *                      from selectionArgs, in order that they appear in the selection. The values
+     *                      will be bound as Strings.
+     * @return The number of updated rows
+     */
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int rowsUpdated;
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE:
+                rowsUpdated = db.update(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+
+            case REVIEW:
+                rowsUpdated = db.update(
+                        MovieContract.ReviewEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+
+            case VIDEO:
+                rowsUpdated = db.update(
+                        MovieContract.VideoEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
+    }
+
+    /**
+     * This method is used for mass insertion into the database
+     *
+     * @param uri    The content:// URI of the insertion request.
+     * @param values An array of sets of column_name/value pairs to add to the database. This must not be null.
+     * @return The number of values that were inserted.
+     */
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int returnCount;
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE:
+                db.beginTransaction();
+                returnCount = 0;
+
+                for (ContentValues item : values) {
+                    long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, item);
+                    if (_id != -1) {
+                        returnCount++;
+                    }
+                }
+                db.setTransactionSuccessful();
+                db.endTransaction();
+
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+
+            case REVIEW:
+                db.beginTransaction();
+                returnCount = 0;
+
+                for (ContentValues item : values) {
+                    long _id = db.insert(MovieContract.ReviewEntry.TABLE_NAME, null, item);
+                    if (_id != -1) {
+                        returnCount++;
+                    }
+                }
+                db.setTransactionSuccessful();
+                db.endTransaction();
+
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+
+            case VIDEO:
+                db.beginTransaction();
+                returnCount = 0;
+
+                for (ContentValues item : values) {
+                    long _id = db.insert(MovieContract.VideoEntry.TABLE_NAME, null, item);
+                    if (_id != -1) {
+                        returnCount++;
+                    }
+                }
+                db.setTransactionSuccessful();
+                db.endTransaction();
+
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+}
